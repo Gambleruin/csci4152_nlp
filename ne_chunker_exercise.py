88e236bd13b4762ee0d3b02b596809e1ef8f99e4
Gambleruin <dannylovewargame@gmail.com>
2018-03-27 22:34:08 -0300

    *

--- ne_chunker_exercise.py
+++ ne_chunker_exercise.py
@@ -4,7 +4,7 @@ from nltk.corpus import treebank
 
 chunkd_trees =[ne_chunk(elem) for elem in treebank.tagged_sents()]
 word_fd =FreqDist([' '.join(word for word, pos in tree.leaves()) for tree in chunkd_trees])
-# print("Three most common named entities are: ")
-# print(','.join(word_fd.most_common(3)))
+print("Three most common named entities are: ")
+print(','.join(str(word_fd.most_common(3))))
 # print(word_fd)
 
